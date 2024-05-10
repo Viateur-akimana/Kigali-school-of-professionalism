fa589f9db90ce983a432449fa0e217ca57d293b1
viateur <akimanaviateur94@gmail.com>
2024-05-10 19:43:48 +0200

    company logos

--- src/app/components/CompanyLogos.tsx
+++ src/app/components/CompanyLogos.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import Image from 'next/image';
-import metaLogo from '../../../public/meta-logo';
+import metaLogo from '../../../public/meta-logo.png';
 import googleLogo from '../../../public/google-logo.png';
-import microsoftLogo from '../../../public/'
+import microsoftLogo from '../../../public/microsoft-logo.png'
 
 const CompanyLogos: React.FC = () => {
   return (
     <div className="my-12">
-      <h2 className="text-2xl font-bold mb-6 text-center">Trusted by a handful of companies</h2>
+      <h2 className="text-2xl font-bold text-blue-600 mb-6 text-center">Trusted by a handful of companies</h2>
       <div className="flex justify-center items-center space-x-8">
         <div className="bg-white rounded-lg shadow-md p-4">
-          <Image src="/meta-logo.png" alt="Meta" width={100} height={100} />
+          <Image src={metaLogo} alt="Meta" width={100} height={100} />
         </div>
         <div className="bg-white rounded-lg shadow-md p-4">
-          <Image src="/google-logo.png" alt="Google" width={100} height={100} />
+          <Image src={googleLogo} alt="Google" width={100} height={100} />
         </div>
         <div className="bg-white rounded-lg shadow-md p-4">
-          <Image src="/microsoft-logo.png" alt="Microsoft" width={100} height={100} />
+          <Image src={microsoftLogo} alt="Microsoft" width={100} height={100} />
+         
         </div>
       </div>
     </div>
