14b02f7d87311834d49f71659272b30cb8813529
viateur <akimanaviateur94@gmail.com>
2024-05-14 15:28:09 +0200

    Before first deployment debugging

--- src/app/components/Introduction.tsx
+++ src/app/components/Introduction.tsx
@@ -8,7 +8,7 @@ const Introduction = () => {
       <div className="md:w-1/2 m-10 p-10 md:mb-0 space-y-7">
         <h1 className="text-4xl text-blue-600 font-bold mb-4">IT Solution Group Limited</h1>
         <p className="text-lg opacity-4">
-          The IT Solutions Group is leading the charge towards Rwanda's tech-driven future. With a steadfast commitment to the transformative power of technology, we are driving change in alignment with Vision 2050. Join us in shaping Rwanda's bright and prosperous future!
+          The IT Solutions Group is leading the charge towards Rwanda s tech-driven future. With a steadfast commitment to the transformative power of technology, we are driving change in alignment with Vision 2050. Join us in shaping Rwanda s bright and prosperous future!
         </p>
         <input
           className="bg-gray-100 px-10 py-2 rounded-md md:w-auto border-2 border-blue-700"
