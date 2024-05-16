b32b24209d4a716b3838b13deb48cce54ca661ed
viateur <akimanaviateur94@gmail.com>
2024-05-16 15:12:38 +0200

    Final site

--- src/app/components/Chance.tsx
+++ src/app/components/Chance.tsx
@@ -1,44 +1,44 @@
-import Card from './Card';
-import experienceImg from "../../../public/experience.png";
-import internshipImg from "../../../public/internship.png";
-import opportunitiesImg from "../../../public/opportunity.png";
+// import Card from './Card';
+// import experienceImg from "../../../public/experience.png";
+// import internshipImg from "../../../public/internship.png";
+// import opportunitiesImg from "../../../public/opportunity.png";
 
-const Chance: React.FC = () => {
-  return (
-    <div className="bg-white py-6 border-t-2">
-      <div className="container mx-auto px-2">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-center items-center">
-          <Card
-            title="Opportunities"
-            description="Job mastery class"
-            imageSrc={opportunitiesImg.src}
-            imageAlt="Opportunities"
-            imageWidth={auto}
-            imageHeight={auto}
+// const Chance: React.FC = () => {
+//   return (
+//     <div className="bg-white py-6 border-t-2">
+//       <div className="container mx-auto px-2">
+//         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-center items-center">
+//           <Card
+//             title="Opportunities"
+//             description="Job mastery class"
+//             imageSrc={opportunitiesImg.src}
+//             imageAlt="Opportunities"
+//             imageWidth={auto}
+//             imageHeight={auto}
     
-          />
-          <Card
-            title="OUR INTERNERNSHIP HUB"
-            description="Learn from our technical experts"
-            imageSrc={internshipImg.src}
-            imageAlt="Internship Hub"
-            imageWidth={auto}
-            imageHeight={auto}
+//           />
+//           <Card
+//             title="OUR INTERNERNSHIP HUB"
+//             description="Learn from our technical experts"
+//             imageSrc={internshipImg.src}
+//             imageAlt="Internship Hub"
+//             imageWidth={auto}
+//             imageHeight={auto}
         
-          />
-          <Card
-            title="EXPERIANCE HUB"
-            description="Gain IT experience through our experience hub"
-            imageSrc={experienceImg.src}
-            imageAlt="Experience Hub"
-            imageWidth={auto}
-            imageHeight={auto}
+//           />
+//           <Card
+//             title="EXPERIANCE HUB"
+//             description="Gain IT experience through our experience hub"
+//             imageSrc={experienceImg.src}
+//             imageAlt="Experience Hub"
+//             imageWidth={auto}
+//             imageHeight={auto}
         
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Chance;
+// export default Chance;
