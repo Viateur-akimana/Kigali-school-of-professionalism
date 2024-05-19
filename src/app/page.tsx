351d78dbcd22b6b28e0af52e1416e94ad0e0018b
viateur <akimanaviateur94@gmail.com>
2024-05-19 20:45:06 +0200

    UPdated chance components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Introduction from './components/Introduction';
 import Choose from './components/Choose';
 
 import Main from './components/Main'
-// import Chance from './components/Chance';
+import Chance from './components/Chance';
 
 const HomePage: React.FC = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <Introduction/>
       <Choose/>
-      {/* <Chance/> */}
+      <Chance/>
       <Main/>
     </div>
   );
