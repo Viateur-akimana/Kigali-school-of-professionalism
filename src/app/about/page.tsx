d1ef42af20dd2642d6ef90e0f4eb36b4fa331413
viateur <akimanaviateur94@gmail.com>
2024-06-10 21:27:06 +0200

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Link from 'next/link';
+'use client'
+
 import image1 from '../../../public/mission.jpg'
 import Image from 'next/image'
 
@@ -10,25 +10,24 @@ const JobPage: React.FC = () => {
       <div className="text-center mb-10 border-b-2">
         <h1 className="text-3xl flex mx-auto justify-center text-blue-600 font-bold">Who we are?</h1>
         <p className="mb-10 text-gray-700 mx-12 my-6 text-xl">
-        IT Solutions Group is a pioneering facilitator entity dedicated to empowering 
-         Students from technology schools to lead the country s tech revolution in alignment
-          with Vision 2050. We are committed to nurturing a modern tech-savvy generation, fostering 
-          innovation, and providing impactful technology solutions for individuals, 
-        enterprises, and large companies across diverse industries.
+          IT Solutions Group is a pioneering facilitator entity dedicated to empowering
+          Students from technology schools to lead the country s tech revolution in alignment
+          with Vision 2050. We are committed to nurturing a modern tech-savvy generation, fostering
+          innovation, and providing impactful technology solutions for individuals,
+          enterprises, and large companies across diverse industries.
         </p>
       </div>
       <div className="flex flex-col md:flex-row items-center mb-8">
         <div className="md:w-1/2 m-10 p-10 md:mb-0 space-y-7 text-gray-600">
           <h1 className="text-4xl text-blue-600 font-bold mb-4">Our mission</h1>
           <p className="text-lg opacity-4">
-            <div className='my-4 text-xl text-gray-800'> <p>Our mission at IT 
+            <div className='my-4 text-xl text-gray-800'> <p>Our mission at IT
               Solutions Group is to leverage our expertise and passion for technology
-               to transform challenges into innovative solutions, driving development
-                and progress. We are dedicated to harnessing the power of technology to
-                 empower individuals, businesses, and communities, making a tangible and positive
-                  impact on Rwandan society and beyond.</p></div>
+              to transform challenges into innovative solutions, driving development
+              and progress. We are dedicated to harnessing the power of technology to
+              empower individuals, businesses, and communities, making a tangible and positive
+              impact on Rwandan society and beyond.</p></div>
 
-           
           </p>
         </div>
         <div className="relative">
@@ -42,9 +41,9 @@ const JobPage: React.FC = () => {
         <div className="p-6 bg-white rounded-lg mx-auto justify-center border-t-2 shadow-lg">
           <h1 className="text-2xl flex mx-auto justify-center text-blue-600 font-bold">Our Vision</h1>
           <p className="mb-4 text-gray-600 mx-6 my-6">
-          Our vision at IT Solutions Group is to democratize access to technology for all Rwandans, creating an inclusive space where everyone can benefit and thrive. We strive to bridge the digital divide by empowering individuals and communities through technology, fostering innovation, and unlocking sustainable growth and prosperity opportunities.
+            Our vision at IT Solutions Group is to democratize access to technology for all Rwandans, creating an inclusive space where everyone can benefit and thrive. We strive to bridge the digital divide by empowering individuals and communities through technology, fostering innovation, and unlocking sustainable growth and prosperity opportunities.
           </p>
-          
+
         </div>
       </div>
     </div>
