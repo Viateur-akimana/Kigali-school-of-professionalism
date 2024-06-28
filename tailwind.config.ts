7af02d14191f6efc0908f887ebec597f6162a43f
viateur <akimanaviateur94@gmail.com>
2024-06-28 21:49:58 +0200

    updated animations and hovers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,29 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        aurora: "aurora 60s linear infinite",
       },
-     
-    },
-  },
-  animation: {
-    spotlight: "spotlight 2s ease .75s 1 forwards",
-  },
-  keyframes: {
-    spotlight: {
-      "0%": {
-        opacity: 0,
-        transform: "translate(-72%, -62%) scale(0.5)",
-      },
-      "100%": {
-        opacity: 1,
-        transform: "translate(-50%,-40%) scale(1)",
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
       },
     },
   },
+  
   plugins: [],
 };
 export default config;
