85cbff6c4989034f1260f0b0bf282040aedcdea2
viateur <akimanaviateur94@gmail.com>
2024-07-05 08:45:04 +0200

    updated admin panel folder structure

--- src/app/pages/admin/index.tsx
+++ src/app/pages/admin/index.tsx
@@ -1,42 +1,47 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar';
 import Navbar from '../../components/Navbar';
-import AnalyticsChart from '../../components/AnalyticsChart';
-import DashboardCard from '../../components/DashboardCard';
-import JobsTable from '../../components/JobsTable';
-import PostsPagination from '../../components/PostsPagination';
-import { Code, LayoutDashboard, Newspaper, User } from 'lucide-react';
+import AnalyticsChart from '../../components/analytics/AnalyticsChart';
+import DashboardCard from '../../components/dashboard/DashboardCard';
+import JobsTable from '../../components/jobs/JobsTable';
+import { Folder, MessageCircle, Newspaper, User } from 'lucide-react';
 
 const AdminDashboard: React.FC = () => {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      <Sidebar />
-      <div className="flex flex-col flex-grow">
-        <Navbar />
-        <div className="flex flex-col p-6">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
+    <>
+      <Navbar />
+      <div className='flex'>
+        <div className='hidden md:block h-[100vh] w-[300px]'>
+          <Sidebar />
+        </div>
+        <div className='p-5 w-full md:max-w-[1140px]'>
+          <div className='flex flex-col md:flex-row justify-between gap-5 mb-5'>
             <DashboardCard
-              title="Jobs"
+              title='Jobs'
               count={10}
-              icon={<LayoutDashboard className='h-10 w-10 text-slate-500' />}
+              icon={<Newspaper className='text-slate-500' size={72} />}
             />
             <DashboardCard
-              title="Users"
+              title='Users'
               count={500}
-              icon={<User className='h-10 w-10 text-slate-500' />}
+              icon={<User className='text-slate-500' size={72} />}
             />
             <DashboardCard
-              title="Posts"
-              count={30}
-              icon={<Newspaper className='h-10 w-10 text-slate-500' />}
+              title='Categories'
+              count={12}
+              icon={<Folder className='text-slate-500' size={72} />}
+            />
+            <DashboardCard
+              title='Comments'
+              count={1200}
+              icon={<MessageCircle className='text-slate-500' size={72} />}
             />
           </div>
           <AnalyticsChart />
-          <JobsTable limit={5} title="Recent Jobs" />
-          <PostsPagination />
+          <JobsTable title='Latest Jobs' limit={5} />
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
