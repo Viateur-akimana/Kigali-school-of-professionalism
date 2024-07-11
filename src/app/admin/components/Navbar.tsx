f203f75c27d60b22a682ca673780ac69c4ce461d
viateur <akimanaviateur94@gmail.com>
2024-07-11 15:19:02 +0200

    making navbar layout

--- src/app/admin/components/Navbar.tsx
+++ src/app/admin/components/Navbar.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import logo from '../../../../public/Vector.png'
+import Image from 'next/image';
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import Link from 'next/link';
+
 
 const Navbar = () => {
   return (
-    <div className='bg-primary dark:bg-slate-700' >Navbar</div>
+    <div className='bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between text-white' >
+      <Link href='/'>
+      <Image 
+      src={logo}
+      width={40}
+      alt='admin logo'
+      />
+      </Link>
+      <Avatar>
+  <AvatarImage src="https://github.com/shadcn.png" />
+  <AvatarFallback className='Text-black' >AV</AvatarFallback>
+</Avatar>
+
+    </div>
   )
 }
 
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar"
 export default function AdminDashboard() {
   return (
     <div className="admin-dashboard">
-        <Navbar />
       <Dashboard />
     </div>
   )
