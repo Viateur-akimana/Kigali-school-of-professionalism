c3c3d4f7f7aa6bf8b2fd87e8cc0154b92f184cc4
viateur <akimanaviateur94@gmail.com>
2024-07-14 16:09:36 +0200

    updated cn for deployment

--- src/app/(client)/components/Testimonials.tsx
+++ src/app/(client)/components/Testimonials.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
--- src/app/(client)/ui/TextGenerateEffect.tsx
+++ src/app/(client)/ui/TextGenerateEffect.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect, useRef } from "react";
 import { motion, stagger, useAnimate } from "framer-motion";
-import { cn } from "../utils/cn";
+import { cn } from "../../utils/cn";
 
 export const TextGenerateEffect = ({
   words,
