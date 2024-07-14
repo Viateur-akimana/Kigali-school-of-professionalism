5bb9ace73b97e74211f66b434d15ebd667d21af4
viateur <akimanaviateur94@gmail.com>
2024-07-14 15:56:51 +0200

    Implemenated job representation

--- src/app/admin/jobs/JobsPage.tsx
+++ src/app/admin/jobs/JobsPage.tsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import {
+  Table,
+  TableCaption,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
+import React from "react";
+import Link from "next/link";
+import jobs from "../../utils/data";
+import Job from "../types/jobs";
 
-const JobsTable = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface JobsTableProps {
+  limit?: number;
+  title?: string;
 }
 
-export default JobsTable
+const JobsTable = ({ limit, title }: JobsTableProps) => {
+  return (
+    <div className="mt-10">
+      <h3 className="text-2xl mb-4 font-semibold">{title ? title : "Posts"}</h3>
+      <Table>
+        <TableCaption>A list of recent posts</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead className="hidden md:table-cell">Company</TableHead>
+            <TableHead className="hidden md:table-cell text-right">
+              Date
+            </TableHead>
+            <TableHead>JobsType</TableHead>
+            <TableHead>Location</TableHead>
+            <TableHead>View</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {jobs.map((job) => (
+            <TableRow key={job.id}>
+              <TableCell>{job.title}</TableCell>
+              <TableCell className="hidden md:table-cell">
+                {job.company}
+              </TableCell>
+              <TableCell className="text-right hidden md:table-cell">
+                {job.date}
+              </TableCell>
+              <TableCell className="text-right hidden md:table-cell">
+                {job.jobType}
+              </TableCell>
+              <TableCell className="text-right hidden md:table-cell">
+                {job.location}
+              </TableCell>
+              <TableCell>
+                <Link href={`/jobs/edit/${job.id}`}>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs">
+                    Edit
+                  </button>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default JobsTable;
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,10 @@
 import Dashboard from './Dashboard'
-import Navbar from "./components/Navbar"
+import JobsTable from './jobs/JobsPage'
 export default function AdminDashboard() {
   return (
     <div className="admin-dashboard">
       <Dashboard />
+      <JobsTable/>
     </div>
   )
 }
