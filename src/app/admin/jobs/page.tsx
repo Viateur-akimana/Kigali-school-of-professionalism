c5d552a87fba73cb9ade29b946cee3f03450ef29
Viateur-akimana <akimanaviateur94@gmail.com>
2024-08-27 20:52:25 +0200

    Adding delete button

--- src/app/admin/jobs/page.tsx
+++ src/app/admin/jobs/page.tsx
@@ -46,9 +46,12 @@ const JobsTable = ({ limit, title }: JobsTableProps) => {
               <TableCell className="hidden md:table-cell">{job.jobType}</TableCell>
               <TableCell className="hidden md:table-cell">{job.location}</TableCell>
               <TableCell>
-                <Link href={`/admin/jobs/edit/${job.id}`}>
+                <Link href={`/admin/jobs/edit/${job.id}`} className="flex space-x-4">
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs">
                     Edit
+                  </button>
+                  <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded text-xss">
+                    Delete
                   </button>
                 </Link>
               </TableCell>
