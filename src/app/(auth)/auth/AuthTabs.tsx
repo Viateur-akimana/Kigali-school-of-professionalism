1c7d910dc1e9a2f0beb26003f94f9d6fe7cd7aaa
Viateur-akimana <akimanaviateur94@gmail.com>
2024-08-28 10:08:48 +0200

    remmoving theme toggler in authentication login page

--- src/app/(auth)/auth/AuthTabs.tsx
+++ src/app/(auth)/auth/AuthTabs.tsx
@@ -1,17 +1,17 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import LoginForm from "./LoginForm";
+  import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+  import LoginForm from "./LoginForm";
 
-const AuthTabs = () => {
-  return (
-    <Tabs defaultValue="login" className="w-[400px]">
-      <TabsList>
-        <TabsTrigger value="login">Login</TabsTrigger>
-      </TabsList>
-      <TabsContent value="login">
-        <LoginForm />
-      </TabsContent>
-    </Tabs>
-  );
-};
+  const AuthTabs = () => {
+    return (
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="login">Login</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <LoginForm />
+        </TabsContent>
+      </Tabs>
+    );
+  };
 
-export default AuthTabs;
+  export default AuthTabs;
--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import ModeToggler from "../admin/components/ThemeToggler";
+
 
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
@@ -6,9 +6,6 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     <html>
       <body>
     <div className='h-[100vh] flex items-center justify-center relative'>
-      <div className='absolute bottom-5 right-0 text-white'>
-        <ModeToggler />
-      </div>
       {children}
     </div>
     </body>
