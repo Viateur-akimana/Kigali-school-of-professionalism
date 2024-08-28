1c83d6df824c65ca19385453aec5ca8010eddd7a
Viateur-akimana <akimanaviateur94@gmail.com>
2024-08-28 15:00:49 +0200

    Socket.IO server setup

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,12 @@
+"use client"
+import { Component as SiteViewsChart } from './components/SiteViewsChart';
 import Dashboard from './Dashboard'
 import JobsTable from './jobs/page'
 export default function AdminDashboard() {
   return (
     <div className="admin-dashboard">
       <Dashboard />
+      <SiteViewsChart/>
       <JobsTable/>
     </div>
   )
