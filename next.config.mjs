a080d35394047f1cb9d905f7ba6001d2d824c4aa
Viateur-akimana <akimanaviateur94@gmail.com>
2024-09-01 13:27:56 +0200

    trying to config socket with nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {protocol: "https", hostname:"www.pexels.com"}
-        ]
-    }
-};
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['socket.io'],
+  
+  },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  webpack(config) {
+    config.resolve.alias['@api'] = './src/app/api';
+    return config;
+  }
+}
 
 export default nextConfig;
--- server.ts
+++ server.ts
@@ -1,20 +1,19 @@
-const { createServer } = require('http');
-const next = require('next');
-const { initializeSocket } = require("./server/socket-server");
+import { createServer } from 'http';
+import next from 'next';
+import { initializeSocket } from "./server/socket-server";
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = createServer((req: any, res: any) => {
+  const server = createServer((req, res) => {
     handle(req, res);
   });
 
+  const io = initializeSocket(server);  // Initialize Socket.io with the HTTP server
 
-  initializeSocket(server);
-
-  server.listen(3000, (err: any) => {
+  server.listen(3000, (err?: any) => {
     if (err) throw err;
     console.log('> Ready on http://localhost:3000');
   });
