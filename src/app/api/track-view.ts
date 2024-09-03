677e3bda68c5971345b212379d67bdfe82a36d87
Viateur-akimana <akimanaviateur94@gmail.com>
2024-09-03 21:45:41 +0200

    corrected database in get site views page

--- src/app/api/track-view.ts
+++ src/app/api/track-view.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
 import { getIo } from '../../../server/socket-server';
+import db from '@/lib/db';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
